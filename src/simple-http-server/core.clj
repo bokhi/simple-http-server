e22ef55f9792b6297332bc697c12b8cd500b4dba
Martin Boissier <martin.boissier@gmail.com>
2020-09-03 09:36:01 +0800

    created endpoints, add simple log

--- src/simple-http-server/core.clj
+++ src/simple-http-server/core.clj
@@ -1,4 +1,35 @@
 (ns simple-http-server.core
   (:require ;; [org.httpkit.sni-client :as sni-client]
-            [org.httpkit.server :as http-kit])
+   [org.httpkit.server :as server]))
+
+(defn d [arg]
+  (println arg)
+  arg)
+
+(defn health-check [req]
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    ""})
+
+(defn docs [req]
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    (slurp "/path/index.html")}
   )
+
+(defn enrich [req])
+
+(defn routing [req]
+  (d (case (:uri (d req))
+       "/health-check" (health-check req)
+       "/docs" (docs req)
+       "/enrich" (enrich req))))
+
+(defn main [arg]
+  (server/run-server #'routing {:port arg}))
+
+(comment
+
+  (main 8888)
+
+  (main 8887))
